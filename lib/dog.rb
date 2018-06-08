9751ccc3a70ef75b6326919a717cd8df4188db9e
Samantha Cassity <cassityclaire@gmail.com>
2018-06-08 17:38:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,15 @@
 # dog.rb
 class Dog 
-  def bark=(dog_bark)
-    @this_dogs_bark=bark
-  end
+
   def bark
-    @this_dogs_bark
+    puts "Woof!"
   end
-def sit=(dog_sit)
-  @this_dogs_sit= sit
+
+
 end
 def sit
   @this_dogs_sit
 end
 end
-woof=Dog.new 
-woof.bark="Woof!"
-puts  "#{woof.bark}"
-sitting=Dog.new 
-sitting.sit="sitting"
-puts "The Dog is #{sitting.sit}"
+
 
